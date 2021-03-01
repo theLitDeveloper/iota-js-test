51c5dfcb9f4f7e9e6c6fbc9e795cdc5dd9ab2574
Stephan Schrader <stephan.schrader@posteo.de>
2021-03-01 09:48:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const Iota = require('@iota/core');
+const Converter = require('@iota/converter');
+
+const iota = Iota.composeAPI({
+    provider: 'https://nodes.devnet.iota.org:443',
+});
+
+const depth = 3;
+const minimumWeightMagnitude = 9;
+const address =
+    'HEQLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWOR99D';
+
+const seed =
+    'PUEOTSEITFEVEWCWBTSIZM9NKRGJEIMXTULBACGFRQK9IMGICLBKW9TTEVSDQMGWKBXPVCBMMCXWMNPDX';
+
+const message = JSON.stringify({ message: 'Hello IOTA world' });
+const messageInTrytes = Converter.asciiToTrytes(message);
+
+const transfers = [
+    {
+        value: 0,
+        address: address,
+        message: messageInTrytes,
+    },
+];
+
+iota.prepareTransfers(seed, transfers)
+    .then((trytes) => {
+        return iota.sendTrytes(trytes, depth, minimumWeightMagnitude);
+    })
+    .then((bundle) => {
+        console.log(bundle[0].hash);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
